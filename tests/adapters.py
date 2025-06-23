878ffb0b008ddb3c23e3c74c6306a29bdf966e62
simpissa <matthewzhao420@gmail.com>
2025-06-23 14:05:20 -0500

    train bpe

--- tests/adapters.py
+++ tests/adapters.py
@@ -9,6 +9,7 @@ import numpy.typing as npt
 import torch
 from torch import Tensor
 
+from cs336_basics.bpe import BPE
 
 
 def run_linear(
@@ -588,4 +589,10 @@ def run_train_bpe(
                 representing that <token1> was merged with <token2>.
                 Merges are ordered by order of creation.
     """
-    raise NotImplementedError
+    bpe = BPE()
+    result = bpe.train_bpe(
+        input_path,
+        vocab_size, 
+        special_tokens,
+    )
+    return result
